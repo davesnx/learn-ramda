406db6848223913be5a3a85fe849b58026326ed5
David Sancho <dsnxmoreno@gmail.com>
2019-05-10 12:54:01 +0200

    Updates

---
+++ precache-manifest.78c7fbe12cf4cca80147365639c630b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eadca4cf331daebdb981",
+    "url": "/learn-ramda/static/js/runtime~main.3e53a01f.js"
+  },
+  {
+    "revision": "2fba0637eaf05d53406c",
+    "url": "/learn-ramda/static/js/main.1138e547.chunk.js"
+  },
+  {
+    "revision": "af9a1d1528663543835c",
+    "url": "/learn-ramda/static/js/3.a8f3deff.chunk.js"
+  },
+  {
+    "revision": "9531ad6f17bb7913495f",
+    "url": "/learn-ramda/static/js/2.f3b89b64.chunk.js"
+  },
+  {
+    "revision": "b96510d81f50a090029dd3d45fa2eb9e",
+    "url": "/learn-ramda/index.html"
+  }
+];
