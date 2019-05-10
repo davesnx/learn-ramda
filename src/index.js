8e3a2b333a7421cb9558fbb3616e32e53a3b04e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-10 09:23:37 +0000

    Deploy davesnx/learn-ramda to github.com/davesnx/learn-ramda.git:gh-pages

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from 'react'
+import { render } from 'react-dom'
+import { Global, css } from '@emotion/core'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import App from './App.jsx'
+import data from './data'
+import colors from './colors'
+
+const normalize = css`
+  @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,600');
+
+  * {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html, body {
+    font-family: 'IBM Plex Mono', monospace;
+    margin: 0;
+    padding: 0;
+    font-size: 18px;
+    line-height: 1.7;
+    color: ${colors.black};
+  }
+
+  a{
+    color: ${colors.blue};
+    transition: 0.2s;
+
+    &:hover { color: ${colors.black}; }
+  }
+`
+
+render(
+  <Fragment>
+  <Global styles={normalize}/>
+  <BrowserRouter hashType='noslash'>
+    <Route
+      path='/:type?/:action?'
+      basename='/learn-ramda'
+      render={props => <App data={data} {...props} />}
+    />
+  </BrowserRouter>
+  </Fragment>,
+  document.getElementById('root')
+)
