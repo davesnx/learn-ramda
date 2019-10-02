ecb75ef5bd440a90a27d995cdd876812ddb98aaf
davesnx <davesnx@users.noreply.github.com>
2019-10-02 14:51:51 +0000

    Automated deployment: Wed Oct  2 14:51:51 UTC 2019 ddd246be3ef17daf7d0a83a1f55d6ca46ea4c05b

---
+++ precache-manifest.4891a08d4c50965b784eff1b4ab9bd8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e884210de9a1e3dffefdfc10ce3b5ef6",
+    "url": "/learn-ramda/index.html"
+  },
+  {
+    "revision": "b965fdc1d461b8e7bf75",
+    "url": "/learn-ramda/static/js/2.1e6af847.chunk.js"
+  },
+  {
+    "revision": "3b8f06c22825a7c8976f",
+    "url": "/learn-ramda/static/js/3.82b134a8.chunk.js"
+  },
+  {
+    "revision": "560026bd901c440162cc",
+    "url": "/learn-ramda/static/js/main.cbbfe8ee.chunk.js"
+  },
+  {
+    "revision": "202c4b062739bbe46cab",
+    "url": "/learn-ramda/static/js/runtime-main.4d6fd373.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learn-ramda/precache-manifest.11d03506d911732e1c79ca1198182c99.js"
+  "/learn-ramda/precache-manifest.4891a08d4c50965b784eff1b4ab9bd8c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/learn-ramda/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learn-ramda/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
